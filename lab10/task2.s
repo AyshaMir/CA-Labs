08c4275826ae5b267927ff672cfcc7933c14f789
Ayesha Mir <am09886@st.habib.edu.pk>
2026-04-10 13:54:30 +0500

    final submission of lab10

--- lab10/task2.s
+++ lab10/task2.s
@@ -1,53 +1,69 @@
-.data
-.text
+.equ led_address,     256
+.equ switch_address,  512
+.equ reset_address,   124
+.equ delay_counter,  10000000
+
+.section .text
 .globl _start
 
 _start:
-    li x4, 256          # x4 = LED address
-    li x5, 512          # x5 = switch address
-    li x6, 124          # x6 = reset address
+    li x4, led_address  #x4 = LED address
+    li x5, switch_address      #x5 = switch address
+    li x6, reset_address       #x6 = reset address
+
+    sw x0, 0(x4)            #clear LEDs at start
+    
 
 idle_state:
-    lw x7, 0(x5)            # read switch value
-    beq x7, x0, idle_state   # if switch value is 0, keep waiting
+    lw x7, 0(x5)        #x7 = switch value
+    beq x7, x0, idle_state  #stay here if switch value is 0
 
-    add x10, x7, x0         # pass switch value in x10
-    jal x1, countdown       # call countdown subroutine
-    j idle_state             # after countdown ends, return to waiting state
+    add x10, x7, x0      #x10 = argument to countdown
+    jal x1, countdown     #call countdown
+    j idle_state            
 
 countdown:
-    addi x2, x2, -16        # make space on stack
-    sw x1, 12(x2)           # save return address
-    sw x8, 8(x2)            # save x8
-    sw x9, 4(x2)            # save x9, reset value
-    sw x18, 0(x2)           # save x18
+    addi x2, x2, -16        #make stack space
+    sw x1, 12(x2)          #save return address
+    sw x8, 8(x2)   #save x8
+    sw x9, 4(x2)         #save x9
+    sw x18, 0(x2)        #save x18
 
-    add x8, x10, x0         # x8 = current countdown value
+    add x8, x10, x0       #x8 = current count value
 
 countdown_loop:
-    lw x9, 0(x6)                # read reset value
-    bne x9, x0, countdown_reset # if reset pressed, reset immediately
+    lw x9, 0(x6)            #read reset
+    bne x9, x0, countdown_reset
+    sw x8, 0(x4)         #display current value on LEDs
 
-    sw x8, 0(x4)                # display current value on LEDs
+    addi x8, x8, -1         #decrement value
+    blt x8, x0, countdown_done
 
-    addi x8, x8, -1             # decrement count
-    blt x8, x0, countdown_done  # if value < 0, stop
+    jal x1, delay_1s  #delay before next number
+    j countdown_loop
 
-    li x18, 10000000            # delay counter
+countdown_reset:
+    sw x0, 0(x4)            #clear LEDs
+
+countdown_done:
+    lw x1, 12(x2)           #restore return address
+    lw x8, 8(x2)           #restore x8
+    lw x9, 4(x2)      #restore x9
+    lw x18, 0(x2)          #restore x18
+    addi x2, x2, 16    #free stack space
+    jalr x0, 0(x1)         #return
+
+delay_1s:
+    addi x2, x2, -8        #save stack space for delay function
+    sw x1, 4(x2)     #save return address
+    sw x18, 0(x2)           #save x18
+    li x18, delay_counter
 
 delay_loop:
     addi x18, x18, -1
     bne x18, x0, delay_loop
-    j countdown_loop
 
-countdown_reset:
-    sw x0, 0(x4)                # clear LEDs
-    j countdown_done
-
-countdown_done:
-    lw x1, 12(x2)           # restore return address
-    lw x8, 8(x2)            # restore x8
-    lw x9, 4(x2)            # restore x9
-    lw x18, 0(x2)           # restore x18
-    addi x2, x2, 16         # free stack space
-    jalr x0, 0(x1)          # return
+    lw x1, 4(x2)            #restore return address
+    lw x18, 0(x2)        #restore x18
+    addi x2, x2, 8          #free stack space
+    jalr x0, 0(x1)       #return
